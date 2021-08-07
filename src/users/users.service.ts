3209811597ceb3915e5469fc36c8d0bf31ca57e6
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-07 06:07:16 +0530

    40-jwt-and-modules.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -49,7 +49,7 @@ export class UsersService {
             const token = jwt.sign({id: user.id}, this.config.get('SECRET_KEY'))
             return {
                 ok: true,
-                token: 'lalalalTOKEN'
+                token,
             }
         } catch (error) {
             return {
