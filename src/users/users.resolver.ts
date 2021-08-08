c13dfd7500fa1ce35b71aa2b8ed48edbd3775f20
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-08 08:46:43 +0530

    49-recap. & did some bug fix in jwt.middleware.ts file in use method

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -13,11 +13,6 @@ export class UsersResolver {
         private readonly usersService: UsersService
     ){}
 
-    @Query(returns => Boolean)
-    hi() {
-        return true;
-    }
-
     @Mutation(returns => CreateAccountOutput)
     async createAccount(@Args('input') createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
