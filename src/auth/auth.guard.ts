9ea14a9429da7c32cac11d935b34a9b20b0ea760
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-08 06:58:13 +0530

    47-authguard.

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivate, Injectable, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    canActivate(context: ExecutionContext) {
+        const gqlContext = GqlExecutionContext.create(context).getContext();
+        const user = gqlContext['user'];
+        if (!user) {
+            return false;
+        }
+        return false;
+    }
+}
