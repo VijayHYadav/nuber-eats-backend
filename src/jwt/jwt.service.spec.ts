fe54afaeac91987131d18da915691743ff14df84
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-11 14:33:05 +0530

    79-jwt-service-test-setup.

---
+++ src/jwt/jwt.service.spec.ts
@@ -0,0 +1,24 @@
+import { Test } from "@nestjs/testing";
+import { CONFIG_OPTIONS } from "src/common/common.constant";
+import { JwtService } from "./jwt.service";
+
+const TEST_KEY = 'testKey';
+
+describe('JwtService', () => {
+  let service: JwtService;
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [JwtService, {
+        provide: CONFIG_OPTIONS,
+        useValue: { privatekey: TEST_KEY }
+      }],
+    }).compile();
+    service = module.get<JwtService>(JwtService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it.todo('sign');
+  it.todo('verify');
+});
