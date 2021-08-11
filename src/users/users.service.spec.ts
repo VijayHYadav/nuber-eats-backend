2e3fbeab4e586fd9286d00cd3b16d50d7199af98
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-11 09:44:35 +0530

    76-editprofile-test-part-two.

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -14,14 +14,15 @@ const mockRepository = () => ({
     findOneOrFail: jest.fn(),
 });
 
-const mockJwtService = {
+const mockJwtService = () => ({
     sign: jest.fn(() => "signed-token-baby"),
     verify: jest.fn()
-}
+});
 
-const mockMailService = {
+
+const mockMailService = () => ({
     sendVerificationEmail: jest.fn(),
-}
+});
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<User>, jest.Mock>>;
 
@@ -46,11 +47,11 @@ describe("UserService", () => {
                 },
                 {
                     provide: JwtService,
-                    useValue: mockJwtService,
+                    useValue: mockJwtService(),
                 },
                 {
                     provide: MailService,
-                    useValue: mockMailService,
+                    useValue: mockMailService(),
                 },
             ]
         }).compile();
@@ -210,6 +211,23 @@ describe("UserService", () => {
             expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(newUser.email, newVerification.code);
         });
 
+        it("should change password", async() => {
+            const editProfileArgs = {
+                userId: 1,
+                input: { password: 'bs@new.com' }
+            };
+            userRepository.findOne.mockResolvedValue({password: 'old'})
+            const result = await service.editProfile(editProfileArgs.userId, editProfileArgs.input);
+            expect(userRepository.save).toHaveBeenCalledTimes(1);
+            expect(userRepository.save).toHaveBeenCalledWith(editProfileArgs.input);
+            expect(result).toEqual({ ok: true })
+        });
+
+        it("should fail on exception", async () => {
+            userRepository.findOne.mockRejectedValue(new Error());
+            const result = await service.editProfile(1, { email: '12' });
+            expect(result).toEqual({ ok: false, error: 'Could not update profile.' })
+        });
     });
 
     it.todo('verifyEmail');
