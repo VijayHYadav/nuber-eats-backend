35390af26a0049f2e6d0e7ca5bffbe043d6ced9c
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-12 07:40:52 +0530

    84-setup-part-one.

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { MailModule } from './mail/mail.module';
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         // validating-configservice
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -40,7 +40,7 @@ import { MailModule } from './mail/mail.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
+      logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [User, Verification]
     }),
     GraphQLModule.forRoot({
