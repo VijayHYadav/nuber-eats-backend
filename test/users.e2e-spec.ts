9abee9d585157779eb14c57ec4b6d3242c8c911d
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-14 08:00:50 +0530

    87-testing-createaccount-part-two.

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,13 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+// "test:e2e": "jest --config ./test/jest-e2e.json --detectOpenHandles"
+jest.mock("got", () => {
+  return {
+    post: jest.fn(),
+  }
+})
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
@@ -40,11 +47,33 @@ describe('AppController (e2e)', () => {
         }
         `,
       })
-      .expect(200)
-      .expect(res => {
-        expect(res.body.data.createAccount.ok).toBe(true);
-        expect(res.body.data.createAccount.error).toBe(null);
-      });
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+        mutation {
+          createAccount(input: {
+            email: "${EMAIL}",
+            password: "12345",
+            role: Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `,
+      })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
     });
   });
 
