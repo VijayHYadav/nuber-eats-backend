87edaca5b1f556ff13fce9e4295c5b27d8db66ac
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-16 09:49:33 +0530

    100-roles-recap.

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -9,7 +9,7 @@ export class JwtMiddleware implements NestMiddleware {
   constructor(
     private readonly jwtService: JwtService,
     private readonly userService: UsersService,
-  ) {}
+  ) { }
   async use(req: Request, res: Response, next: NextFunction) {
     if ('x-jwt' in req.headers) {
       const token = req.headers['x-jwt'];
@@ -21,7 +21,7 @@ export class JwtMiddleware implements NestMiddleware {
             req['user'] = user;
           }
         }
-      } catch (e) {}
+      } catch (e) { }
     }
     next();
   }
