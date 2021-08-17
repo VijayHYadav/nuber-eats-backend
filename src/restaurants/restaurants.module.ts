c3e113b269d0d645093e165d58e1034c2cf343bf
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-18 04:44:57 +0530

    103-restaurant-part-three.

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
+import { CategoryRepository } from './repositories/category.repository';
 import { RestaurantResolver } from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])], // basically this allows you to import typeormmodule for a specific feature in this case it is Restaurant.
+  imports: [TypeOrmModule.forFeature([Restaurant, CategoryRepository])], // basically this allows you to import typeormmodule for a specific feature in this case it is Restaurant.
   providers: [RestaurantResolver, RestaurantService],
 })
 export class RestaurantsModule { }
