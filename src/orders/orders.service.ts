7131f71daaee0d204e3e3a55f09c979cb7c76320
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-21 12:06:58 +0530

    124-create-order-part-six.

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -22,47 +22,63 @@ export class OrderService {
     ) { }
 
     async createOrder(customer: User, { restaurantId, items }: CreateOrderInput): Promise<CreateOrderOutput> {
-        const restaurant = await this.restaurants.findOne(restaurantId);
-        if (!restaurant) {
-            return {
-                ok: false,
-                error: "Restaurant not found",
-            };
-        }
-        for (const item of items) {
-            const dish = await this.dishes.findOne(item.dishId);
-            if (!dish) {
-                // abort this whole thing
+        try {
+            const restaurant = await this.restaurants.findOne(restaurantId);
+            if (!restaurant) {
                 return {
                     ok: false,
-                    error: 'Dish not found.'
-                }
+                    error: "Restaurant not found",
+                };
             }
-            console.log(`Dish price: ${dish.price}`)
-            for (const itemOption of item.options) {
-                const dishOption = dish.options.find(dishOption => dishOption.name === itemOption.name);
-                if (dishOption) {
-                    if (dishOption.extra) {
-                        console.log(`$USD + ${dishOption.extra}`);
-                    } else {
-                        const dishOptionChoice = dishOption.choices.find(optionChoice => optionChoice.name === itemOption.choice);
-                        if (dishOptionChoice) {
-                            if (dishOptionChoice.extra) {
-                                console.log(`$USD + ${dishOptionChoice.extra}`)
+            let orderFinalPrice = 0;
+            const orderItems: OrderItem[] = [];
+            for (const item of items) {
+                const dish = await this.dishes.findOne(item.dishId);
+                if (!dish) {
+                    // abort this whole thing
+                    return {
+                        ok: false,
+                        error: 'Dish not found.'
+                    }
+                }
+                let dishFinalPrice = dish.price;
+                for (const itemOption of item.options) {
+                    const dishOption = dish.options.find(dishOption => dishOption.name === itemOption.name);
+                    if (dishOption) {
+                        if (dishOption.extra) {
+                            dishFinalPrice = dishFinalPrice + dishOption.extra;
+                        } else {
+                            const dishOptionChoice = dishOption.choices.find(optionChoice => optionChoice.name === itemOption.choice);
+                            if (dishOptionChoice) {
+                                if (dishOptionChoice.extra) {
+                                    dishFinalPrice = dishFinalPrice + dishOptionChoice.extra;
+                                }
                             }
                         }
                     }
                 }
+                orderFinalPrice = orderFinalPrice + dishFinalPrice;
+                const orderItem = await this.ordersItems.save(this.ordersItems.create({
+                    dish,
+                    options: item.options,
+                }));
+                orderItems.push(orderItem);
             }
-            // await this.ordersItems.save(this.ordersItems.create({
-            //     dish,
-            //     options: item.options,
-            // }))
+            const order = await this.orders.save(this.orders.create());
+            this.orders.create({
+                customer,
+                restaurant,
+                total: orderFinalPrice,
+                items: orderItems,
+            });
+            return {
+                ok: true,
+            }
+        } catch {
+            return {
+                ok: false,
+                error: "Could not create order."
+            }
         }
-        // const order = await this.orders.save(this.orders.create());
-        // this.orders.create({
-        //     customer,
-        //     restaurant
-        // });
     }
 }
