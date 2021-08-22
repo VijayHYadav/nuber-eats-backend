39bcba6d25710b28648c34b06b337063c6548992
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-22 15:11:40 +0530

    132-pub-sub.

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { Dish } from './restaurants/entities/dish.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
+import { CommonModule } from './common/common.module';
 
 @Module({
   imports: [
@@ -78,6 +79,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
     MailModule,
     RestaurantsModule,
     OrdersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,4 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { PubSub } from 'graphql-subscriptions';
+import { PUB_SUB } from './common.constant';
 
-@Module({})
+// NOTE: There should be only and just one pubsub for whole application.
+const pubsub = new PubSub();
+@Global()
+@Module({
+    providers: [
+        {
+            provide: PUB_SUB,
+            useValue: pubsub,
+        },
+    ],
+    exports: [PUB_SUB],
+})
 export class CommonModule {}
+
+/* NOTE ON new PubSub()
+Note that the default PubSub implementation is intended for demo purposes.
+It only works if you have a single instance of your server and doesn't scale beyond a couple of connections. 
+For production usage you'll want to use one of the PubSub implementations backed by an external store. (e.g. Redis)
+*/
