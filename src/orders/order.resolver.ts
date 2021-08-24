8b85e0d792edaf4d143b16afee319fb86f7de1f6
vijayyadav10 <vijay.yadav@newvisionsoftware.in>
2021-08-24 06:53:51 +0530

    138-orderupdates-part-one.

--- src/orders/order.resolver.ts
+++ src/orders/order.resolver.ts
@@ -4,12 +4,13 @@ import { PubSub } from "graphql-subscriptions";
 import { filter } from "rxjs";
 import { AuthUser } from "src/auth/auth-user.decorator";
 import { Role } from "src/auth/role.decorator";
-import { NEW_COOKED_ORDER, NEW_PENDING_ORDER, PUB_SUB } from "src/common/common.constant";
+import { NEW_COOKED_ORDER, NEW_ORDER_UPDATE, NEW_PENDING_ORDER, PUB_SUB } from "src/common/common.constant";
 import { User } from "src/users/entities/user.entity";
 import { CreateOrderInput, CreateOrderOutput } from "./dtos/create-order.dto";
 import { EditOrderInput, EditOrderOutput } from "./dtos/edit-order.dto";
 import { GetOrderInput, GetOrderOutput } from "./dtos/get-order.dto";
 import { GetOrdersInput, GetOrdersOutput } from "./dtos/get-orders.dto";
+import { OrderUpdatesInput } from "./dtos/order-updates.dto";
 import { Order } from "./entities/order.entity";
 import { OrderService } from "./orders.service";
 
@@ -71,4 +72,9 @@ export class OrderResolver {
         return this.pubSub.asyncIterator(NEW_COOKED_ORDER)
     }
 
+    @Subscription(returns => Order)
+    @Role(['Any'])
+    orderUpdates(@Args('input') OrderUpdatesInput: OrderUpdatesInput) {
+        return this.pubSub.asyncIterator(NEW_ORDER_UPDATE)
+    }
 }
